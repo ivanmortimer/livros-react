d2466a5187e4927676d9831dc30ec45d57fd07ab
Ivan Mortimer <ivanmortimer@yahoo.com.br>
2024-10-27 20:53:19 -0300

    (B2.2) Second commit on Branch 2. 'livro', 'excluir' e 'nomeEditora' como atributos de 'this.state' da class 'ListaLivros'.

--- src/LivroLista.js
+++ src/LivroLista.js
@@ -4,17 +4,27 @@ import ControleLivro from "./controle/ControleLivros";
 import ControleEditora from "./controle/ControleEditora";
 
 class LivroLista extends React.Component {
-    controleLivro = new ControleLivro;
-    controleEditora = new ControleEditora;
-}
-
-class LinhaLivro extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             livro: this.props.livro,
             excluir: this.props.excluir,
-            nomeEditora: ControleEditora.getNomeEditora(this.livro.codEditora)
-        };
+            nomeEditora: ControleEditora.getNomeEditora(livro.codEditora)
+        }
     }
+    controleLivro = new ControleLivro;
+    controleEditora = new ControleEditora;
+
+    LinhaLivro(props) {
+        const [livro, setLivro] = useState(this.props.livro);
+        const [excluir, setExcluir] = useState(this.props.excluir);
+        const [nomeEditora, setNomeEditora] = useState(ControleEditora.getNomeEditora(livro.codEditora));
+    
+        return (
+            <tr>
+                <td>{ livro.codigo }</td>
+            </tr>
+        );
+    }
+    
 }
